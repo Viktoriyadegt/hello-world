489a8dbf896895a69cd19708d65d2395f6c92bdd
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2024-10-05 18:02:47 +0300

    recursion

---
+++ task_186.js
@@ -0,0 +1,9 @@
+function persistence(num) {
+
+    const str = num.toString();
+    if (str.length === 1) {
+        return 0;
+    }
+    const nextNum = str.split('').reduce((a, b) => a * b, 1);
+    return 1 + persistence(nextNum);
+}
