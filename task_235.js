e07d42e303049866d0ce8a4a12b43c0b70a39055
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2024-11-23 19:00:35 +0300

    repeat_trim_split

---
+++ task_235.js
@@ -0,0 +1,15 @@
+//
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+//
+// Add the value "codewars" to the array websites 1,000 times.
+//
+//
+//
+//______________________________________________________________________________________________________________________
+
+
+
+
+let websites = 'codewars '.repeat(1000).trim().split(' ')
+
+
