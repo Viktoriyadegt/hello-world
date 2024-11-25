8072c9e1f8ed377a8b72adc11c27b6abc27992ce
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2024-11-25 22:30:19 +0300

    filter

---
+++ task_237.js
@@ -0,0 +1,17 @@
+//
+// Time to test your basic knowledge in functions! Return the odds from a list:
+//
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+//
+//______________________________________________________________________________________________________________________
+
+
+
+
+function odds(values) {
+    return values.filter(f => f % 2);
+}k
+
+
+
