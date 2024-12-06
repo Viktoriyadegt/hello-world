0c58a231323461e765cee1e839c584e59cd5cef1
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2024-12-06 09:15:01 +0300

    codePointAt_charCodeAt

---
+++ task_248.js
@@ -0,0 +1,10 @@
+
+//
+// Get ASCII value of a character.
+//
+// For the ASCII table you can refer to http://www.asciitable.com/
+//______________________________________________________________________________________________________________________
+
+function getASCII(c) {
+    return c.codePointAt(0)  //charCodeAt
+}
