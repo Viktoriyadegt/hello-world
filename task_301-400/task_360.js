411b0f4a5fa3d5341dcf6441fc21745e6157ab43
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2025-03-28 12:37:43 +0300

    Switcheroo fix description

--- task_301-400/task_360.js
+++ task_301-400/task_360.js
@@ -1,5 +1,6 @@
 // Description:
-// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b
+// (change a to b and vice versa). Leave any incidence of c untouched.
 //
 // Example:
 //
