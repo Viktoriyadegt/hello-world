c073ca7f03df80781af9b05ec1c1d95f7bd05969
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2025-05-17 16:32:43 +0300

    task2

--- leetcode/task_2.js
+++ leetcode/task_2.js
@@ -3,7 +3,12 @@
 // where each digits[i] is the ith digit of the integer.
 // The digits are ordered from most significant to least significant in left-to-right order.
 // The large integer does not contain any leading 0's.
-// Increment the large integer by one and return the resulting array of digits:)
+// Increment the large integer by one and return the resulting array of digits.
+// Constraints:
+//
+// 1 <= digits.length <= 100
+// 0 <= digits[i] <= 9
+// digits does not contain any leading 0's.
 //======================================================================================================================
 
 /**
@@ -20,6 +25,7 @@ const plusOne = function (digits) {
     }
     return [1, ...digits]
 };
+
 
 //test =================================================================================================================
 
